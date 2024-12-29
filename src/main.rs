bdb3dc32f1b824c3aa40d57b054e143e7ad580db
Jörg Volbers <joerg@joergvolbers.de>
2024-12-29 13:57:14 +0100

    Add simple logging using a macro

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,36 @@
 use std::{
-    error::Error,
-    net::{self, SocketAddr, TcpListener, TcpStream},
-    io::{prelude::*, BufReader}};
+    error::Error, io::{prelude::*, BufReader}, net::{SocketAddr, TcpListener, TcpStream}};
 
-// TODO Add timestamp to output
+use chrono::{DateTime, Local};
+
 // TODO Continue tutorial
 // https://doc.rust-lang.org/book/ch20-01-single-threaded.html
 
-fn get_request(mut stream: TcpStream) -> Vec<String> {
+/// Get a timestamp
+///
+fn timestamp() -> String {
+    let time: DateTime<Local> = Local::now();
+    return format!("{}", time.format("%Y-%m-%d %H:%M:%S%.6f"));
+}
+
+
+// https://danielkeep.github.io/practical-intro-to-macros.html
+// https://veykril.github.io/tlborm/decl-macros/macros-methodical.html
+
+#[macro_export]
+macro_rules! info {
+    // Match: a repeating sequence $();
+    // matching one or more times, separated by a comma: $(),*
+    // which repeats an expression, captured as a variable "arg":
+    // ($arg:expr)
+    // -> ( $($arg:expr),+ )
+    
+    ( $($arg:expr),+  ) => { println!("[{}] {}", timestamp(), format_args!($($arg),+) ) }
+}
+
+/// Read the stream until an empty line.
+///
+fn get_request(stream: TcpStream) -> Vec<String> {
     let reader = BufReader::new(&stream);    
     return reader.lines()
         .map(|result| result.unwrap())
@@ -15,10 +38,12 @@ fn get_request(mut stream: TcpStream) -> Vec<String> {
         .collect();
 }
 
+
 fn main() -> Result<(), Box<dyn Error>> {    
 
     // Address to listen to
     let backend = SocketAddr::from(([127,0,0,1], 8993));
+    info!("Listening to {backend}");
 
     // Open connection
     let listener = TcpListener::bind(&backend)?;
@@ -27,11 +52,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                println!("Connection established! {stream:?}");
-                println!("{:#?}", get_request(stream));
+                info!("{:#?}", get_request(stream));
             },
             Err(e) => {
-                println!("Something went wrong: {e:?}");
+                info!("Something went wrong: {e:?}");
             }
         }
     }
