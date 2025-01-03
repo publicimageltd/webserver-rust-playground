68dbaf22eebd1819a44a8d1b03b96fd9360d578d
Jörg Volbers <joerg@joergvolbers.de>
2025-01-04 00:42:18 +0100

    Add one more level of indirection since we have two header types

--- src/header.rs
+++ src/header.rs
@@ -1,19 +1,52 @@
-///! Headers
+///! Define a header type and some standard headers
+///!
+///! To get a header, use `header::predefined()` or `header::custom`
+///!
+
 use std::fmt;
 
-/// (Some) Header names according to the HTTP standard
+// Public API
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Header {
+    kind: Kind,
+}
+
+pub fn predefined(header: Predefined) -> Header {
+    Header { kind: Kind::Standard(header), }
+}
+
+pub fn custom(s: String) -> Header {
+    Header { kind: Kind::Custom(s), }
+}
+
+// Impl
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum Kind {
+    Standard(Predefined),
+    Custom(String),
+}
+
+impl fmt::Display for Kind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Kind::Standard(predefined) => write!(f, "{}", predefined),
+            Kind::Custom(s) => write!(f, "{}", s),
+        }
+    }
+}
+
 macro_rules! define_standardheaders {
     ( $( ($name:ident, $val:literal), )+   
     ) =>
-    { #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-      enum StandardHeader {
-      $( $name, )+
-      }
+    { #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+        pub enum Predefined {            
+           $( $name, )+
+        }
         
-      impl fmt::Display for StandardHeader {
+      impl fmt::Display for Predefined {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
           match *self {
-             $( StandardHeader::$name  => write!(f, $val), )+
+             $( Predefined::$name  => write!(f, $val), )+
           }
         }
       }
@@ -35,7 +68,7 @@ mod test {
 
     #[test]
     fn header_macro_as_string() {
-        assert_eq!(format!("{}", StandardHeader::ContentLength),
+        assert_eq!(format!("{}", Predefined::ContentLength),
             "content-length")
     }
 }
