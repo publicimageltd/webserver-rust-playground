4184d290a26660d9581cc2a4bf3056f68d2b435e
Jörg Volbers <joerg@joergvolbers.de>
2025-01-03 20:33:06 +0100

    And another way of playing around

--- src/header.rs
+++ src/header.rs
@@ -6,38 +6,27 @@ use std::collections::HashMap;
 /// (Some) Header names according to the HTTP standard
 // We use a lifetime parameter to allow for a constant with global
 // (static) lifetime
-#[derive(Debug,PartialEq)]
+#[derive(Debug,PartialEq,Hash,Eq)]
 pub struct HeaderName<'a>(&'a str);
 
-pub const CONTENT_LENGTH: &HeaderName = &HeaderName("content-length");
-pub const CONTENT_TYPE:   &HeaderName = &HeaderName("content-type");
-pub const DATE:           &HeaderName = &HeaderName("date");
-pub const FROM:           &HeaderName = &HeaderName("from");
-pub const REFERER:        &HeaderName = &HeaderName("referer");
+pub const CONTENT_LENGTH: HeaderName = HeaderName("content-length");
+pub const CONTENT_TYPE:   HeaderName = HeaderName("content-type");
+pub const DATE:           HeaderName = HeaderName("date");
+pub const FROM:           HeaderName = HeaderName("from");
+pub const REFERER:        HeaderName = HeaderName("referer");
 
-/// A header, both for responses or requests.
-///
-/// Note that HTTP 2 allows non-string headers.
+struct Headers<'a>(HashMap<HeaderName<'a>,String>);
 
-#[derive(Debug,PartialEq)]
-struct Header<'a> {
-    name: HeaderName<'a>,
-    // TODO Non-empty string, actually.
-    value: &'a str,
-}
 
-impl <'a> Header<'a> {
-    fn custom(name: &'a str, val: &'a str) -> Header<'a> {
-        Header { name: HeaderName(name),
-            value: val,
-        }
+
+impl<'a> Headers<'a> {
+    fn standard(&mut self, name: &'static HeaderName, val: String) {
+        HashMap::insert(&mut self.0, HeaderName(name.0), val);
     }
-    fn standard(name: &'static HeaderName, val: &'a str) -> Header<'a> {
-        Header { name: HeaderName(name.0),
-            value: val }
+    fn custom(&mut self, name: &'a str, val: String) {
+        self.0.insert(HeaderName(name), val);
     }
 }
-
 
 // -----------------------------------------------------------
 
@@ -46,10 +35,8 @@ mod test {
     use super::*;
 
     #[test]
-    fn create_custom_headers() {
-        let custom = Header::custom("date", "today");
-        let standard = Header::standard(DATE, "today");
-        assert_eq!(custom, standard);
+    fn empty() {
+        ()
     }
 }
 
